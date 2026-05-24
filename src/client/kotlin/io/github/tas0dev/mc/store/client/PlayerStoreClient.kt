422a03d089e3549a051b8c4662300fe862c8de42
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-05-24 16:40:33 +0900

    Initialize commit

---
+++ src/client/kotlin/io/github/tas0dev/mc/store/client/PlayerStoreClient.kt
@@ -0,0 +1,9 @@
+package io.github.tas0dev.mc.store.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object PlayerStoreClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/io/github/tas0dev/mc/store/client/PlayerStoreDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.tas0dev.mc.store.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object PlayerStoreDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
